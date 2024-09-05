66519c698db63591f993130453f685fe6cabe43b
jkpeyi <jkpeyi@gmail.com>
2024-09-05 12:37:53 +0100

    wip

--- src/components/MyView.vue
+++ src/components/MyView.vue
@@ -1,81 +1,105 @@
 <template>
-    <section class="view">
-        <div id="frame" :style="style">
-            <component v-for="element in elements" :is="element"></component>
-        </div>
-        <div class="menu_section">
-            <div class="menu_list">
-                <button @click="addRect">Rectangle</button>
-                <button @click="addText">Texte</button>
-                <button>Cercle</button>
-            </div>
-        </div>
-    </section>
+  <section class="view">
+    <div
+      id="frame"
+      :style="` width: ${props.width}px ; height: ${props.height}px ; `"
+    >
+      <component
+        :bgColor="'orange'"
+        :id="`draggable${index}`"
+        @click="selectElement(`draggable${index}`)"
+        v-for="(element, index) in elements"
+        :is="element"
+      ></component>
+
+   
+    </div>
+
+    <div class="menu_section">
+      <div class="menu_list">
+        <button @click="addRect">Rectangle</button>
+        <button @click="addText">Texte</button>
+        <button>Cercle</button>
+      </div>
+    </div>
+  </section>
 </template>
-  
+
 <script setup lang="ts">
 import { computed, ref, watch } from "vue";
 import Rect from "../components/widgets/Rect.vue";
 import Text from "../components/widgets/Text.vue";
 
-const props = defineProps(['color' , 'label' , 'rotation' , 'width' , 'height' , 'border']) 
-const elements = ref([]) ;
-
-const style = computed(()=>{
-    return `background-color: ${props.color}; 
+const props = defineProps([
+  "color",
+  "label",
+  "rotation",
+  "width",
+  "height",
+  "border",
+]);
+const elements = ref([]);
+const selectedElement = ref<HTMLElement>();
+const num = ref(0);
+const style = computed(() => {
+  return `background-color: ${props.color}; 
     transform: rotateZ(${props.rotation}deg) ; 
-    width: ${props.width}px ; 
-    height: ${props.height}px ; 
-    border: 2px solid ${props.border}`
-})
+   
+    border: 2px solid ${props.border}`;
+});
 function addRect() {
-    elements.value.push(Rect)
-    console.log(addRect);
-    
+  elements.value.push(Rect);
+  //console.log(addRect);
 }
 function addText() {
-    elements.value.push(Text)
+  elements.value.push(Text);
 }
-watch(elements.value , ()=>{
-    console.log(elements);
-    
-})
+
+function selectElement(ref: string) {
+  
+    selectedElement.value = document.getElementById(ref)
+      ?.children[0] as HTMLElement;
+}
+watch(style, () => {
+  selectedElement.value?.setAttribute("style", style.value);
+});
 </script>
 
 <style scoped>
-.view{
-    width: 100%;
-    height: 100vh;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    gap: 30px;
-    align-items: center;
-    perspective: 1000px;
+.view {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 30px;
+  align-items: center;
+  perspective: 1000px;
 }
-#frame{
-    width: 350px;
-    height: 200px;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    padding-top: 30px;
-    transition: transform 0.5s ease;
-    transform-origin: center;
+#frame {
+  width: 350px;
+  height: 200px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding-top: 30px;
+  transition: transform 0.5s ease;
+  transform-origin: center;
+  background-color: #fff;
 }
-.menu_section{
-    width: 30%;
+.menu_section {
+  width: 30%;
 }
-.menu_list{
-    width: 100%;
-    display: flex;
-    justify-content: space-between;
+.menu_list {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
 }
-button{
-    color: #fff;
-    padding: 10px;
-    background-color: inherit;
-    border: 1px solid grey;
+button {
+  color: #fff;
+  padding: 10px;
+  background-color: inherit;
+  border: 1px solid grey;
 }
 </style>
--- src/components/widgets/Rect.vue
+++ src/components/widgets/Rect.vue
@@ -1,10 +1,15 @@
 <template>
     <Draggable>
-        <div class="rectangle"></div>
+        <div class="rectangle" :style="`background-color: ${bgColor}`" ></div>
     </Draggable>
 </template>
 <script setup lang="ts">
 import Draggable from "../Draggable.vue";
+
+defineProps(['bgColor'])
+
+
+
 </script>
 <style scoped>
 .rectangle{
